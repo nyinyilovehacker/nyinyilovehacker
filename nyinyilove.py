c464fd22466344a99241430b6f2814f8c2c1bb0a
nyinyilovehacker <107629849+nyinyilovehacker@users.noreply.github.com>
2022-06-27 01:29:15 +0530

    nyinyilove.py
    apt update 
    apt upgrade
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    git clone https://github.com/nyinyilovehacker/techconsoe.git
    pip install bs4
    pip install requests
    pip2 install mechanize
    cd nyinyilove
    ls
    python nyinyilove.py

---
+++ nyinyilove.py
@@ -0,0 +1,31 @@
+# draw rainbow sprial 
+# turtle graphics
+
+
+import turtle
+from  import turtlert *    
+
+turtie.title("rainbow sprial")
+speed(15)
+bgcolor("black")
+r, g,    b =  255, 0,  0
+
+for i in range(255*2):
+     colormode(255)
+     if   i>255//3:               if codit
+           g+=3
+     elif i>255*2//3:     
+           r-=3
+     elif  i>255:
+           b+=3
+     elif   i>255*4//3:
+           g-=3
+     elif    i>255*5//3:
+           r+=3
+     elif  :
+           b-=3
+     fd(50+i)
+     rt(91)
+     pencolor(r, g, b)
+     
+      
