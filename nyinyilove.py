d57435644d14bbb16542756f406a8f0c7b9d7b0e
nyinyilovehacker <107629849+nyinyilovehacker@users.noreply.github.com>
2022-06-27 02:17:36 +0530

    Update and rename NyiNyilovehacker.py to nyinyilove.py
    apt update 
    apt upgrade
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    git clone https://github.com/nyinyilovehacker/techconsoe.git
    pip install bs4
    pip install requests
    pip2 install mechanize
    cd nyinyilove
    ls
    python nyinyilove.py

--- nyinyilove.py
+++ nyinyilove.py
@@ -1,11 +1,7 @@
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-reload(sys)
-sys.setdefaultencoding('utf8')
- 
 
+#!/usr/local/bin/python
+import os, sys
+...
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
